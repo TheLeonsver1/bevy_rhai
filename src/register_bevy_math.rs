0865f763b19fb9f40cb1577bf59db25c7003a2e4
Leon <sverdleon@gmail.com>
2021-07-18 18:04:38 +0300

    Added BVec, Quats and more float fns

--- src/register_bevy_math.rs
+++ src/register_bevy_math.rs
@@ -8,45 +8,55 @@ macro_rules! register_float_vecn {
             .register_fn("distance", $vec_type::distance)
             .register_fn("distance_squared", $vec_type::distance_squared)
             .register_fn("dot", $vec_type::dot)
+            .register_fn("normalize", $vec_type::normalize)
+            .register_fn("normalize_or_zero", $vec_type::normalize_or_zero)
             .register_fn("cross", $vec_type::cross);
     };
     ($engine:ident, $vec_type:ident,$scalar:ident,2) => {
         $engine
             .register_fn("distance", $vec_type::distance)
             .register_fn("distance_squared", $vec_type::distance_squared)
+            .register_fn("normalize", $vec_type::normalize)
+            .register_fn("normalize_or_zero", $vec_type::normalize_or_zero)
             .register_fn("dot", $vec_type::dot)
     };
 }
 
 macro_rules! register_vecn {
     ($engine:ident, $vec_type:ident,$scalar:ident) => {
-        $engine
-            .register_type::<$vec_type>()
-            //Instantiating
-            .register_fn("new", $vec_type::new)
-            .register_fn("default", $vec_type::default)
-            //Math
-            .register_fn("add", $vec_type::add)
-            .register_fn("add_assign", $vec_type::add_assign)
-            .register_fn("sub", $vec_type::sub)
-            .register_fn("sub_assign", $vec_type::sub_assign)
-            .register_fn("mul", <$vec_type as Mul<$scalar>>::mul)
-            .register_fn("mul_assign", <$vec_type as MulAssign<$scalar>>::mul_assign)
-            .register_fn("mul", <$vec_type as Mul<$vec_type>>::mul)
-            .register_fn(
-                "mul_assign",
-                <$vec_type as MulAssign<$vec_type>>::mul_assign,
-            )
-            .register_fn("div", <$vec_type as Div<$scalar>>::div)
-            .register_fn("div_assign", <$vec_type as DivAssign<$scalar>>::div_assign)
-            .register_fn("div", <$vec_type as Div<$vec_type>>::div)
-            .register_fn(
-                "div_assign",
-                <$vec_type as DivAssign<$vec_type>>::div_assign,
-            )
-            //Convenience
-            .register_fn("extend", $vec_type::extend)
-            .register_fn("to_array", $vec_type::to_array);
+        $engine.register_type::<$vec_type>();
+        //Instantiating;
+        $engine.register_fn("new", $vec_type::new);
+        $engine.register_fn("default", $vec_type::default);
+        //Math;
+        $engine.register_fn("add", $vec_type::add);
+        $engine.register_fn("add_assign", $vec_type::add_assign);
+        $engine.register_fn("sub", $vec_type::sub);
+        $engine.register_fn("sub_assign", $vec_type::sub_assign);
+        $engine.register_fn("mul", <$vec_type as Mul<$scalar>>::mul);
+        $engine.register_fn("mul_assign", <$vec_type as MulAssign<$scalar>>::mul_assign);
+        $engine.register_fn("mul", <$vec_type as Mul<$vec_type>>::mul);
+        $engine.register_fn(
+            "mul_assign",
+            <$vec_type as MulAssign<$vec_type>>::mul_assign,
+        );
+
+        $engine.register_fn("div", <$vec_type as Div<$scalar>>::div);
+        $engine.register_fn("div_assign", <$vec_type as DivAssign<$scalar>>::div_assign);
+        $engine.register_fn("div", <$vec_type as Div<$vec_type>>::div);
+        $engine.register_fn(
+            "div_assign",
+            <$vec_type as DivAssign<$vec_type>>::div_assign,
+        );
+        //Convenience
+        $engine.register_fn("extend", $vec_type::extend);
+        $engine.register_fn("to_array", $vec_type::to_array);
+        //Comparing
+        $engine.register_fn("cmple", $vec_type::cmple);
+        $engine.register_fn("cmplt", $vec_type::cmplt);
+        $engine.register_fn("cmpge", $vec_type::cmpge);
+        $engine.register_fn("cmpgt", $vec_type::cmpgt);
+        $engine.register_fn("cmpeq", $vec_type::cmpeq);
     };
 }
 macro_rules! register_vecn_conversion_to_f32 {
@@ -98,6 +108,14 @@ macro_rules! register_vec2 {
     };
 }
 
+macro_rules! register_bvecn {
+    ($engine:ident, $vec_type:ident) => {
+        $engine.register_type::<$vec_type>();
+        $engine.register_fn("all", $vec_type::all);
+        $engine.register_fn("all", $vec_type::any);
+    };
+}
+
 pub fn register_bevy_math_types(engine: &mut Engine) {
     register_vec3!(engine, Vec3, f32);
     register_float_vecn!(engine, Vec3, f32, 3);
@@ -116,4 +134,28 @@ pub fn register_bevy_math_types(engine: &mut Engine) {
     register_signed!(engine, IVec2, i32);
     register_vec2!(engine, UVec2, u32);
     register_vecn_conversion_to_f32!(engine, UVec2, u32);
+
+    engine.register_type::<Quat>();
+    //Instantiating
+    engine.register_fn("from_xyzw", Quat::from_xyzw);
+    engine.register_fn("default", Quat::default);
+    //Math
+    engine.register_fn("add", Quat::add);
+    engine.register_fn("sub", Quat::sub);
+    engine.register_fn("mul", <Quat as Mul<f32>>::mul);
+    engine.register_fn("div", <Quat as Div<f32>>::div);
+    engine.register_fn("mul_quat", Quat::mul_quat);
+    engine.register_fn("angle_between", Quat::angle_between);
+    engine.register_fn("conjugate", Quat::conjugate);
+    engine.register_fn("dot", Quat::dot);
+    engine.register_fn("from_euler", Quat::from_euler);
+    engine.register_fn("length", Quat::length);
+    engine.register_fn("length_squared", Quat::length_squared);
+    engine.register_fn("normalize", Quat::normalize);
+    engine.register_fn("is_normalized", Quat::is_normalized);
+    engine.register_fn("to_euler", Quat::to_euler);
+    engine.register_fn("to_axis_angle", Quat::to_axis_angle);
+
+    register_bvecn!(engine, BVec3);
+    register_bvecn!(engine, BVec2);
 }
